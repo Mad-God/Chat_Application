9d98a4a76f22cd5c42f01ac1c8c995c9bde084e8
satyam <stmsng2001@gmail.com>
2022-07-20 10:57:11 +0530

    Changed the other tests in chat_test to use factories for each instance that requires a user record

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -21,24 +21,24 @@ def test_index(client, get_chat_group):
 
  
 
-# def test_group_create(client, get_chat_group, new_user):
-#     index_url = urls.reverse("chat:home")
-#     client.force_login(user = User.objects.get(username=new_user.username))
-#     resp = client.post(index_url, get_chat_group)
-#     assert resp.status_code == 302
-#     assert str(resp.url).lower() == f"/chat/{get_chat_group['slug']}".lower()
-#     assert ChatGroup.objects.count() == 1
+def test_group_create(client, get_chat_group, new_user):
+    index_url = urls.reverse("chat:home")
+    client.force_login(user = User.objects.get(username=new_user.get().username))
+    resp = client.post(index_url, get_chat_group)
+    assert resp.status_code == 302
+    assert str(resp.url).lower() == f"/chat/{get_chat_group['slug']}".lower()
+    assert ChatGroup.objects.count() == 1
 
  
 
-# def test_group_apply(client, create_chat_group, new_user):
-#     index_url = urls.reverse("chat:apply-membership", kwargs={"name":"ChatGroup"})
-#     client.force_login(user = User.objects.get(username=new_user.username))
-#     resp = client.get(index_url)
-#     assert ChatGroup.objects.count() == 1
-#     assert resp.status_code == 302
-#     assert resp.url == f"/"
-#     assert Member.objects.count() == 1
+def test_group_apply(client, create_chat_group, new_user):
+    index_url = urls.reverse("chat:apply-membership", kwargs={"name":"ChatGroup"})
+    client.force_login(user = User.objects.get(username=new_user.get().username))
+    resp = client.get(index_url)
+    assert ChatGroup.objects.count() == 1
+    assert resp.status_code == 302
+    assert resp.url == f"/"
+    assert Member.objects.count() == 1
 
 
 @pytest.mark.db
@@ -59,39 +59,45 @@ def test_group_accept(client, create_chat_group, new_user):
 
         
 
-# def test_group_deny(client, create_chat_group, new_user, get_user):
-#     chat_group = create_chat_group
-#     index_url = urls.reverse("chat:deny-membership", kwargs={"name":chat_group.name, 'user':new_user.id})
-#     Member.objects.create(group = chat_group, user = new_user, accepted = False)
-#     chat_group.admin.add(get_user.id)
-#     client.force_login(user = User.objects.get(username=get_user.username))
-#     resp = client.get(index_url)
-#     assert resp.status_code == 302
-#     assert resp.url.lower() == f"/chat/{chat_group.slug}".lower()
-#     assert Member.objects.count() == 0
+def test_group_deny(client, create_chat_group, new_user, get_user):
+    chat_group = create_chat_group
+    user = new_user.get()
+    admin = new_user.get()
+    index_url = urls.reverse("chat:deny-membership", kwargs={"name":chat_group.name, 'user':user.id})
+    Member.objects.create(group = chat_group, user = user, accepted = False)
+    chat_group.admin.add(admin.id)
+    client.force_login(user = User.objects.get(username=admin.username))
+    resp = client.get(index_url)
+    assert resp.status_code == 302
+    assert resp.url.lower() == f"/chat/{chat_group.slug}".lower()
+    assert Member.objects.count() == 0
 
 
 
-# def test_group_revoke(client, create_chat_group, new_user, get_user):
-#     chat_group = create_chat_group
-#     index_url = urls.reverse("chat:revoke-membership", kwargs={"name":chat_group.name, 'user':new_user.id})
-#     Member.objects.create(group = chat_group, user = new_user, accepted = True)
-#     chat_group.admin.add(get_user.id)
-#     client.force_login(user = User.objects.get(username=get_user.username))
-#     resp = client.get(index_url)
-#     assert resp.status_code == 302
-#     assert resp.url.lower() == f"/chat/{chat_group.slug}".lower()
-#     assert Member.objects.count() == 0
+def test_group_revoke(client, create_chat_group, new_user):
+    chat_group = create_chat_group
+    user = new_user.get()
+    admin = new_user.get()
+    index_url = urls.reverse("chat:revoke-membership", kwargs={"name":chat_group.name, 'user':user.id})
+    Member.objects.create(group = chat_group, user = user, accepted = True)
+    chat_group.admin.add(admin.id)
+    client.force_login(user = User.objects.get(username=admin.username))
+    resp = client.get(index_url)
+    assert resp.status_code == 302
+    assert resp.url.lower() == f"/chat/{chat_group.slug}".lower()
+    assert Member.objects.count() == 0
 
 
 
-# def test_group_invite(client, create_chat_group, new_user, get_user):
-#     chat_group = create_chat_group
-#     index_url = urls.reverse("chat:invite-membership", kwargs={"group_id":chat_group.id,})
-#     assert Member.objects.count() == 0
-#     client.force_login(user = User.objects.get(username=new_user.username))
-#     resp = client.get(index_url)
-#     assert resp.status_code == 302
-#     assert Member.objects.count() == 1
-#     assert str(resp.url).lower() == f"/chat/{chat_group.slug}".lower()
+def test_group_invite(client, create_chat_group, new_user, get_user):
+    chat_group = create_chat_group
+    user = new_user.get()
+    admin = new_user.get()
+    index_url = urls.reverse("chat:invite-membership", kwargs={"group_id":chat_group.id,})
+    assert Member.objects.count() == 0
+    client.force_login(user = User.objects.get(username=user.username))
+    resp = client.get(index_url)
+    assert resp.status_code == 302
+    assert Member.objects.count() == 1
+    assert str(resp.url).lower() == f"/chat/{chat_group.slug}".lower()
 
